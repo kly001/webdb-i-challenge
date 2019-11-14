82f1f7c3194bb8f44d5d7e34a5f2013606d60f02
kly001 <karenly1961@gmail.com>
2019-11-14 18:34:23 -0500

    DELETE working

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -60,4 +60,16 @@ router.put('/:id', (req, res) => {
     })
     });
 
+    router.delete('/:id', async (req, res) => {
+        const { id } = req.params;
+    
+     try {
+            const count = await db.del().from('accounts').where({ id });
+            count ? res.status(200).json({ deleted: count })
+                : res.status(404).json({ message: 'invalid id' });
+        } catch (err) {
+            res.status(500).json({ message: 'database error', error: err });
+        }
+    });
+
 module.exports = router;
