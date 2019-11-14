26a8593c81f67d320806378b57b70fb6db1799d4
kly001 <karenly1961@gmail.com>
2019-11-14 18:31:56 -0500

    PUT working

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -43,4 +43,21 @@ router.post('/', async (req, res) => {
 })
 
 
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+    
+    db('accounts').where({id}).update(changes)
+    .then(count => {
+        if (count) {
+            res.status(200).json({updated: count});
+        } else {
+            res.status(404).json ({message: 'Invalid id'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message:'db problem'})
+    })
+    });
+
 module.exports = router;
